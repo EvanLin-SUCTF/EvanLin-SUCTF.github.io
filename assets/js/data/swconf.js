2796f3b4537dc0c1891da002dc9d02ab9f71b008
EvanLin-SUCTF <evanlin1123@foxmail.com>
2026-03-13 18:18:55 +0800

    Init challenge

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,22 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1773397087',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
